1720363b6522fcee9bc598536166590d49fdc04f
anton-cid-mejias <anton.cid.mejias@gmail.com>
2020-03-09 17:23:24 +0100

    Added prints

--- p2m/losses.py
+++ p2m/losses.py
@@ -18,15 +18,17 @@ import tensorflow as tf
 from chamfer import *
 
 def laplace_coord(pred, placeholders, block_id):
-	print("pred:", pred.shape)
-	print("placeholders:", placeholders)
-	print("blockid:", block_id)
-
 	vertex = tf.concat([pred, tf.zeros([1,3])], 0)
 	indices = placeholders['lape_idx'][block_id-1][:, :8]
 	weights = tf.cast(placeholders['lape_idx'][block_id-1][:,-1], tf.float32)
 
 	weights = tf.tile(tf.reshape(tf.reciprocal(weights), [-1,1]), [1,3])
+
+	print("Vertex:", vertex)
+	print(vertex.shape)
+	print("Indices:", indices)
+	print(indices.shape)
+
 	laplace = tf.reduce_sum(tf.gather(vertex, indices), 1)
 	laplace = tf.subtract(pred, tf.multiply(laplace, weights))
 	return laplace
